d5e74fecc6d076bb2106b94e9c51d8451061981e
RomanKarasev <roman.karasev@icloud.com>
2022-11-22 23:08:10 +0300

    Initial Commit

---
+++ UTcoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UTcoin
+//
+//  Created by Роман Карасёв on 22.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
